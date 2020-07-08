eb618526260fcd087e02f9fbe6ded4733797820c
Rinat Nurutdinow <jheat@vo-e3.msk.21-school.ru>
2020-07-08 19:38:07 +0300

    first commit

---
+++ get_next_line_utils copy.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheat <jheat@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/07 18:35:10 by jheat             #+#    #+#             */
+/*   Updated: 2020/07/08 18:04:02 by jheat            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char		*ft_strchr(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)(s + i));
+	return (NULL);
+}
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+
+	if (!(str = (char *)malloc(sizeof(char) * size + 1)))
+		return (NULL);
+	str[size] = '\0';
+	while (size--)
+		str[size] = '\0';
+	return (str);
+}
+
+size_t		ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	char		*str;
+	size_t		str_len;
+	size_t		i;
+	size_t		j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	str_len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (str_len + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[str_len] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*buff;
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	buff = (char *)malloc(sizeof(*str) * (i + 1));
+	if (buff == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		buff[i] = str[i];
+		i++;
+	}
+	buff[i] = '\0';
+	return (buff);
+}
